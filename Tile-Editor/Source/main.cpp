3c2dc876d6d2a9aa5682d3507273755c47043ae8
Abhay More <abhay7more@gmail.com>
2024-03-12 20:16:18 +0530

    Added more features Grid Size change Grid thickness change Pen Size change (to colour multiple cells at once)

--- Tile-Editor/Source/main.cpp
+++ Tile-Editor/Source/main.cpp
@@ -3,19 +3,21 @@
 #include <imgui-SFML.h>
 #include <iostream>
 #include <unordered_map>
+#include <string>
 
 template<>
-struct std::hash<std::pair<int, int>> {
-    size_t operator()(const std::pair<int, int> cellCoord) const
+struct std::hash<std::tuple<int, int, int>> {
+    size_t operator()(const std::tuple<int, int, int>  cellCoord) const
     {
-        return std::hash<int>()(cellCoord.first) ^ std::hash<int>()(cellCoord.second);
+        return std::hash<int>()(get<0>(cellCoord)) ^ std::hash<int>()(get<1>(cellCoord)) ^ std::hash<int>()(get<2>(cellCoord));// std::hash<int>()(cellCoord.second.second);
     }
 };
+
 
 class TileLayer
 {
 private:
-    std::unordered_map<std::pair<int, int>, ImVec4> m_cellColors;
+    std::unordered_map<std::tuple<int, int, int>, ImVec4> m_cellColors;
     bool m_isVisible;
 
 public:
@@ -23,13 +25,13 @@ public:
     {
     }
 
-    void setTile(int row, int col, const ImVec4& color) {
-        m_cellColors[std::make_pair(row, col)] = color;
+    void setTile(int pensize, int row, int col, const ImVec4& color) {
+        m_cellColors[std::make_tuple(pensize, row, col)] = color;
     }
 
-    ImVec4 getTile(int row, int col) const
+    ImVec4 getTile(int pensize, int row, int col) const
     {
-        auto it = m_cellColors.find(std::make_pair(row, col));
+        auto it = m_cellColors.find(std::make_tuple(pensize, row, col));
         if (it != m_cellColors.end())
             return it->second;
         else
@@ -73,9 +75,10 @@ public:
     {
     }
 
-    void render(ImDrawList* drawList, int highlightCellX, int highlightCellY, bool showGrid) const {
+    void render(ImDrawList* drawList, ImVec2 cellSize, int highlightCellX, int highlightCellY, bool showGrid, float gridThickness) {
         ImVec2 windowPos = ImGui::GetCursorScreenPos();
 
+        m_cellSize = cellSize;
         //Draws only visible layers. New Layers are drawn on Top of Old ones
         for (const auto& layer : m_tileLayers)
         {
@@ -85,12 +88,15 @@ public:
                 {
                     for (int col = 0; col < m_numCols; ++col)
                     {
-                        ImVec4 cellColor = layer.second.getTile(row, col);
-                        ImVec4 color = { 0,0,0,0 };
-                        float cellX = windowPos.x + col * m_cellSize.x;
-                        float cellY = windowPos.y + row * m_cellSize.y;
-                        drawList->AddRectFilled(ImVec2(cellX, cellY), ImVec2(cellX + m_cellSize.x, cellY + m_cellSize.y), ImGui::ColorConvertFloat4ToU32(cellColor));
+                        for (int i = 8; i <= 128; i *= 2)
+                        {
+                            ImVec4 cellColor = layer.second.getTile(i, row, col);
+                            ImVec4 color = { 0,0,0,0 };
+                            float cellX = windowPos.x + col * i;
+                            float cellY = windowPos.y + row * i;
+                            drawList->AddRectFilled(ImVec2(cellX, cellY), ImVec2(cellX + i, cellY + i), ImGui::ColorConvertFloat4ToU32(cellColor));
 
+                        }
                     }
                 }
             }
@@ -98,20 +104,20 @@ public:
         if (showGrid) {
 
             // Render horizontal grid lines
-            drawList->AddLine(ImVec2(windowPos.x, windowPos.y), ImVec2(windowPos.x + m_canvasSize.x, windowPos.y), IM_COL32(150, 150, 150, 255));
+            drawList->AddLine(ImVec2(windowPos.x, windowPos.y), ImVec2(windowPos.x + m_canvasSize.x, windowPos.y), IM_COL32(150, 150, 150, 255), gridThickness);
             for (float y = windowPos.y + m_cellSize.y - 1; y < windowPos.y + m_canvasSize.y; y += m_cellSize.y) {
-                drawList->AddLine(ImVec2(windowPos.x, y), ImVec2(windowPos.x + m_canvasSize.x, y), IM_COL32(150, 150, 150, 255));
+                drawList->AddLine(ImVec2(windowPos.x, y), ImVec2(windowPos.x + m_canvasSize.x, y), IM_COL32(150, 150, 150, 255), gridThickness);
             }
 
             // Render vertical grid lines
-            drawList->AddLine(ImVec2(windowPos.x, windowPos.y), ImVec2(windowPos.x, windowPos.y + m_canvasSize.y), IM_COL32(150, 150, 150, 255));
+            drawList->AddLine(ImVec2(windowPos.x, windowPos.y), ImVec2(windowPos.x, windowPos.y + m_canvasSize.y), IM_COL32(150, 150, 150, 255), gridThickness);
             for (float x = windowPos.x + m_cellSize.x - 1; x < windowPos.x + m_canvasSize.x; x += m_cellSize.x) {
-                drawList->AddLine(ImVec2(x, windowPos.y), ImVec2(x, windowPos.y + m_canvasSize.y), IM_COL32(150, 150, 150, 255));
+                drawList->AddLine(ImVec2(x, windowPos.y), ImVec2(x, windowPos.y + m_canvasSize.y), IM_COL32(150, 150, 150, 255), gridThickness);
             }
         }
                
     }
-    void setCellColor(int row, int col, const ImVec4& color) {
+    void setCellColor(int pensize, int row, int col, const ImVec4& color) {
 
         //Mouse draws only on selected layer ID.
         //selected layer ID is first searched in TileLayers to be modified.
@@ -120,7 +126,7 @@ public:
             auto it = m_tileLayers.find(m_selectedLayer);
             if (it != m_tileLayers.end())
             {
-                it->second.setTile(row, col, color);
+                it->second.setTile(pensize, row, col, color);
             }
         }
     }
@@ -129,7 +135,8 @@ public:
         ImGui::SetNextWindowSizeConstraints(ImVec2(250, -1), ImVec2(FLT_MAX, -1));
         ImGui::Begin("Layers", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
 
-        for (auto it = m_tileLayers.begin(); it != m_tileLayers.end(); ++it) {
+        for (auto it = m_tileLayers.begin(); it != m_tileLayers.end(); ++it)
+        {
             int layerNumber = it->first; 
             bool isSelected = (m_selectedLayer == layerNumber);
 
@@ -141,7 +148,8 @@ public:
             }
             
         }
-        if (ImGui::Button("Add")) {
+        if (ImGui::Button("Add"))
+        {
 
             for (int i = 1; i <= m_tileLayers.size() + 1; i++)
             {
@@ -155,7 +163,8 @@ public:
         }
         ImGui::SameLine();
 
-        if (ImGui::Button("Delete")) {
+        if (ImGui::Button("Delete"))
+        {
             auto it = m_tileLayers.find(m_selectedLayer);
             if (it != m_tileLayers.end())
             {
@@ -189,10 +198,25 @@ int main()
 
     ImGui::SFML::Init(window);
 
-    ImVec2 canvasSize(800, 640); 
-    ImVec2 cellSize(32, 32); 
+    ImVec2 canvasSize(25 * 32, 20 * 32); 
+    int cellSizePixel = 8;
+    ImVec2 cellSize(cellSizePixel, cellSizePixel);
+
+    //Cell Size init
+    std::string cellSizeLabel[] = {"1x", "2x", "4x", "8x", "16x"};
+    float selectedCellSize = 0;
+
 
     Grid grid(canvasSize, cellSize);
+
+    //Grid Thickness init
+    const char* gridThicknessLabel[] = { "1x", "2x", "3x", "4x" };
+    float selectedGridThickness = 0;
+
+    //Pen Size init
+    const char* penSizeLabel[] = { "1x", "2x", "3x", "4x" };
+    int selectedPenSize = 0;
+    ImVec2 penSize(cellSizePixel, cellSizePixel);
 
     // Default selected color in Color Palette
     ImVec4 selectedColor(1.0f, 0.0f, 0.0f, 1.0f); 
@@ -281,7 +305,7 @@ int main()
         ImGui::BeginChild("GridChild", ImVec2(canvasSize.x, canvasSize.y), false, ImGuiWindowFlags_NoScrollbar);
 
         ImDrawList* drawList = ImGui::GetWindowDrawList();
-        grid.render(drawList, highlightCellX, highlightCellY, showGrid);
+        grid.render(drawList, cellSize, highlightCellX, highlightCellY, showGrid, selectedGridThickness + 1);
         grid.drawLayerWindow();
 
         
@@ -289,26 +313,79 @@ int main()
         {
             windowPos = ImGui::GetCursorScreenPos();
             sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-
+          
             if (mousePos.x >= windowPos.x && mousePos.x < windowPos.x + canvasSize.x &&
                 mousePos.y >= windowPos.y && mousePos.y < windowPos.y + canvasSize.y) {
-                highlightCellX = static_cast<int>((mousePos.x - windowPos.x) / cellSize.x);
-                highlightCellY = static_cast<int>((mousePos.y - windowPos.y) / cellSize.y);
+                
+                for (int i = -1; i <= -1 + selectedPenSize; ++i)
+                {
+                    for (int j = -1; j <= -1 + selectedPenSize; ++j)
+                    {
+                        highlightCellX = static_cast<int>((mousePos.x - windowPos.x) / penSize.x);
+                        highlightCellY = static_cast<int>((mousePos.y - windowPos.y) / penSize.y);
 
-                if(m_leftMouseButtonPressed && ImGui::IsWindowHovered())
-                    grid.setCellColor(highlightCellY, highlightCellX, selectedColor);
-                else if(m_rightMouseButtonPressed && ImGui::IsWindowHovered())
-                    grid.setCellColor(highlightCellY, highlightCellX, {0,0,0,0});
+                        if (m_leftMouseButtonPressed && ImGui::IsWindowHovered())
+                            grid.setCellColor(penSize.x, highlightCellY + i + 1, highlightCellX + j + 1, selectedColor);
+                        else if (m_rightMouseButtonPressed && ImGui::IsWindowHovered())
+                            grid.setCellColor(penSize.x, highlightCellY + i + 1, highlightCellX + j + 1, { 0,0,0,0 });
 
+                    }
+                }
+                
             }
         }
         ImGui::EndChild();
         ImGui::End();
 
         //EDIT PANEL WINDOW
-        ImGui::Begin("Color Palette");
+        ImGui::Begin("Edit Panel");
         ImGui::ColorEdit4("Selected Color", reinterpret_cast<float*>(&selectedColor), ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_AlphaBar);
         ImGui::Checkbox("Show Grid", &showGrid);
+
+
+        // Cell Size
+        ImGui::Text(("Cell Size (x = " + std::to_string(static_cast<int>(cellSize.x)) + ")").c_str());
+        for (int i = 0; i < sizeof(cellSizeLabel) / sizeof(cellSizeLabel[0]); ++i) {
+            ImGui::Selectable(cellSizeLabel[i].c_str(), selectedCellSize == i, ImGuiSelectableFlags_None, ImVec2(15, 0));
+            if (ImGui::IsItemClicked()) {
+                selectedCellSize = i;
+                selectedPenSize = 0;
+                cellSize = ImVec2(cellSizePixel * std::stoi(cellSizeLabel[i], 0), cellSizePixel * std::stoi(cellSizeLabel[i], 0));
+                penSize = cellSize;
+            }
+            ImGui::SameLine();
+            ImGui::Spacing();
+            ImGui::SameLine();
+
+        }
+        ImGui::Spacing();
+
+        // Grid Thickness
+        ImGui::Text(("Grid Thickness (x = " + std::to_string(static_cast<int>(1.0f)) + ")").c_str());
+        for (int i = 0; i < sizeof(gridThicknessLabel) / sizeof(gridThicknessLabel[0]); ++i) {
+            ImGui::Selectable(gridThicknessLabel[i], selectedGridThickness == i, ImGuiSelectableFlags_None, ImVec2(15, 0));
+            if (ImGui::IsItemClicked()) {
+                selectedGridThickness = i;
+            }
+            ImGui::SameLine();
+            ImGui::Spacing();
+            ImGui::SameLine();
+
+        }
+        ImGui::Spacing();
+
+        // Pen Size
+        ImGui::Text(("Pen Size (x = " + std::to_string(static_cast<int>(penSize.x)) + ")").c_str());
+        for (int i = 0; i < sizeof(penSizeLabel) / sizeof(penSizeLabel[0]); ++i) {
+            ImGui::Selectable(penSizeLabel[i], selectedPenSize == i, ImGuiSelectableFlags_None, ImVec2(15, 0));
+            if (ImGui::IsItemClicked()) {
+                selectedPenSize = i;
+            }
+            ImGui::SameLine();
+            ImGui::Spacing(); 
+            ImGui::SameLine();
+
+        }
 
         ImGui::End();
 
